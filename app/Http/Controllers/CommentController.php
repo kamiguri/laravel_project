941713c94d661c7fc35c407009bded4b6aefac46
kamiguri <d.kamiguri@core-tech.jp>
2024-05-29 10:33:53 +0900

    コメントのstoreコントローラを追加

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request, string $id) {
+        $comment = new Comment();
+        $comment->user_id = Auth::id();
+        $comment->content_id = $id;
+        $comment->text = $request->text;
+        $comment->save();
+
+        return route('video.show', ['id' => $id]);
+    }
 }
