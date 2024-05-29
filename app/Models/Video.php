ecc9cc6100d7435afe829691e448143bfd458720
kamiguri <d.kamiguri@core-tech.jp>
2024-05-29 11:31:28 +0900

    videoモデルからコメント一覧を取得できるよう変更

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Video extends Model
 {
@@ -15,4 +16,8 @@ class Video extends Model
         'overview'
     ];
 
+    public function comments(): MorphMany
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
 }
