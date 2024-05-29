473c9947815bf39a7801be1379a103246429751e
nagatacoretech <t.nagata@core-tech.jp>
2024-05-29 17:10:26 +0900

    search with queryparameter ,escape counterplan

--- resources/views/video/index.blade.php
+++ resources/views/video/index.blade.php
@@ -5,17 +5,33 @@
         </h2>
     </x-slot>
     <h1>Video List</h1>
-    <div class="video-list">
-        @foreach($videos as $video)
-            <div class="video-item">
-                <a href="{{route('video.show',$video->id)}}">
-                    <video controls width="400" muted>
-                        <source src="{{ asset($video->path) }}">
-                    </video>
-                    <p>{{$video->title}}</p>
-                </a>
+    <form action="{{route('video.index')}}" method="GET">
+        @csrf
+        <input type="search" name="search" value="{{ request('search')}}">
+        <button>検索</button>
+    </form>
 
-            </div>
-        @endforeach
+    <div class="video-list">
+        @if(isset($video_query))
+            @foreach($video_query as $query)
+                <a href="{{route('video.show',$query->id)}}">
+                    {{$query->title}}
+                    <video controls width="400" muted>
+                        <source src="{{ asset($query->path) }}">
+                    </video>
+                </a>
+            @endforeach
+        @else
+            @foreach($videos as $video)
+                <div class="video-item">
+                    <a href="{{route('video.show',$video->id)}}">
+                        <video controls width="400" muted>
+                            <source src="{{ asset($video->path) }}">
+                        </video>
+                        <p>{{$video->title}}</p>
+                    </a>
+                </div>
+            @endforeach
+        @endif
     <h1><a href="/video/create">動画投稿画面へ</a></h1>
 </x-app-layout>
