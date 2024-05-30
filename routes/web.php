6a2dd2d128e71c7e5fb7d6cc26939cd7bccc21f3
nagatacoretech <t.nagata@core-tech.jp>
2024-05-30 15:04:40 +0900

    guest-user-access and root directory video.index

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,9 @@ use App\Http\Controllers\VideoController;
 use App\Http\Controllers\CommunityController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('video.index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -21,24 +18,23 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/video/index', [VideoController::class, 'index'])->name('video.index');
+Route::get('/video/show/{id}',[VideoController::class,'show'])->name('video.show');
 Route::middleware('auth')->group(function () {
-    Route::get('/video/index', [VideoController::class, 'index'])->name('video.index');
-    Route::post('/video/index',[VideoController::class, 'index'])->name('video.index');
     Route::get('/video/create', [VideoController::class, 'create'])->name('video.create');
     Route::post('/video/store', [VideoController::class, 'store'])->name('video.store');
-    Route::get('/video/show/{id}',[VideoController::class,'show'])->name('video.show');
     Route::post('/video/delete/{id}',[VideoController::class,'delete'])->name('video.delete');
     Route::get('/video/edit/{id}',[VideoController::class,'edit'])->name('video.edit');
     Route::post('/video/update/{id}',[VideoController::class,'update'])->name('video.update');
 });
 
+Route::get('/community/index', [CommunityController::class, 'index'])->name('community.index');
+Route::post('/community/index', [CommunityController::class, 'index'])->name('community.index');
+Route::get('/community/show', [CommunityController::class, 'show'])->name('community.show');
 Route::middleware('auth')->group(function (){
-    Route::get('/community/index', [CommunityController::class, 'index'])->name('community.index');
-    Route::post('/community/index', [CommunityController::class, 'index'])->name('community.index');
     Route::get('/community/create', [CommunityController::class, 'create'])->name('community.create');
     Route::post('/community/create', [CommunityController::class, 'create'])->name('community.create');
     Route::post('/community/store', [CommunityController::class, 'store'])->name('community.store');
-    Route::get('/community/show', [CommunityController::class, 'show'])->name('community.show');
     Route::get('/community/detail/{id}',[CommunityController::class,'detail'])->name('community.detail');
     Route::post('/community/delete/{id}',[CommunityController::class,'delete'])->name('community.delete');
     Route::get('/community/edit/{id}',[CommunityController::class,'edit'])->name('community.edit');
