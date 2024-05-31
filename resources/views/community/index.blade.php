fa709e4429b414f7306740a8b6c5f7f52119c3d3
kamiguri <d.kamiguri@core-tech.jp>
2024-05-31 10:48:46 +0900

    コミュニティ一覧のテキストの位置を調整 コミュニティに境界線と余白を追加

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -5,25 +5,27 @@
         </h1>
     </x-slot>
     <h1>コミュニティへようこそ</h1>
+    <h1><a href="/community/create">コミュニティ投稿画面へ</a></h1>
+    <h1><a href="/community/show">自分の投稿へ</a></h1>
 
     @foreach ($communities as $community)
+    <div class="border rounded w-2/3 p-5 mt-3">
         <a href="{{ route('community.detail', $community->id) }}">
+        <div>
+            <p>{{ $community->users_id }}</p>
+        </div>
         <div>
             @if ($community->path)
             <img src="{{ Storage::url($community->path) }}" alt="Community Image">
-            @else
-                <p>画像は登録されていません</p>
+            {{-- @else
+                <p>画像は登録されていません</p> --}}
             @endif
-        </div>
-        <div>
-            <p>{{ $community->users_id }}</p>
         </div>
         <div>
             <p>{{ $community->com_text }}</p>
         </div>
     </a>
+    </div>
     @endforeach
 
-    <h1><a href="/community/create">コミュニティ投稿画面へ</a></h1>
-    <h1><a href="/community/show">自分の投稿へ</a></h1>
 </x-app-layout>
