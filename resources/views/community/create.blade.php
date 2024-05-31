c381408b360ef7dee26493d8d8cf1397edd0d878
kamiguri <d.kamiguri@core-tech.jp>
2024-05-31 13:35:16 +0900

    コミュニティ新規作成フォームのスタイル、レイアウトを変更

--- resources/views/community/create.blade.php
+++ resources/views/community/create.blade.php
@@ -4,23 +4,24 @@
             {{ __('Community') }}
         </h2>
     </x-slot>
-    <h1>コミュニティ投稿</h1>
-
-    <form action="{{ route('community.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div>
-            <label for="title">投稿内容</label>
-            <input type="text" name="com_text" id="com_text">
+    <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <h1 class="text-lg font-bold">コミュニティ投稿</h1>
+            <form action="{{ route('community.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="mt-3">
+                    <x-input-label for="title" :value="__('投稿内容')" />
+                    <x-textarea name="com_text" id="com_text" class="w-full"></x-textarea>
+                </div>
+                <div class="mt-3">
+                    <x-input-label for="image" :value="__('アップロード画像')" />
+                    <input id="image" type="file" name="image">
+                </div>
+                <div class="mt-3 flex justify-between">
+                    <a href="/community/index"><x-secondary-button>コミュニティ画面へ</x-secondary-button></a>
+                    <x-primary-button type="submit" class="btn btn-primary">アップロード</x-primary-button>
+                </div>
+            </form>
         </div>
-        <div>
-            <label for="image">
-                <p>アップロード画像</p>
-                <input id="image" type="file" name="image">
-            </label>
-        </div>
-        <div>
-            <button type="submit" class="btn btn-primary">アップロード</button>
-        </div>
-    </form>
-    <h1><a href="/community/index">コミュニティ画面へ</a></h1>
+    </div>
 </x-app-layout>
