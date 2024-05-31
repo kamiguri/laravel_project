140dbf15eb22cea2bfd0f5447025a0ab4581ad78
kamiguri <d.kamiguri@core-tech.jp>
2024-05-31 13:58:42 +0900

    コメント編集フォームのスタイル、レイアウトを変更

--- resources/views/comment/edit.blade.php
+++ resources/views/comment/edit.blade.php
@@ -1,10 +1,24 @@
-<div>
-    <form action="{{ route('comment.update', [ 'id' => $comment->id ])}}" method="POST">
-        @csrf
-        @method('PUT')
-        <textarea name="text" placeholder="コメントする..." rows="1">
-            {{ $comment->text }}
-        </textarea>
-        <button type="submit">送信</button>
-    </form>
-</div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('You Tube!!') }}
+        </h2>
+    </x-slot>
+    <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <p class="text-lg font-bold">コメントの編集</p>
+            <form action="{{ route('comment.update', [ 'id' => $comment->id ])}}" method="POST">
+                @csrf
+                @method('PUT')
+                <x-textarea name="text" placeholder="コメントする..." class="w-full" rows="1" required>
+                    {{ $comment->text }}
+                </x-textarea>
+                <div class="flex justify-between">
+                    <x-secondary-button onclick="history.back()">戻る</x-secondary-button>
+                    <x-primary-button>送信</x-primary-button>
+                </div>
+            </form>
+        </div>
+    </div>
+</x-app-layout>
